498c0939912d2374300a810872d6bdb7ce492fdf
UpSetSummerSet <72717739+14-TanKeeLiang@users.noreply.github.com>
2022-01-26 02:19:37 +0800

    Update <Added Script For Player, Coins & Buttons>
    Added scripts for player & coins.
    
    Coin script is just for the coin to have a rotation animation.
    
    Player script is to manage the coin collected and the collision of the coins & the water.
    
    Button script is for changing scenes.

---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private int coinCollected;
+
+    private AudioSource audioSource;
+
+    public AudioClip[] audioClips;
+    public Text coinCollectedTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(coinCollected == 12)
+        {
+            SceneManager.LoadScene("GameWin");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            audioSource.PlayOneShot(audioClips[0]);
+            coinCollected += 1;
+            coinCollectedTxt.text = "CoinCollected: " + coinCollected;
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.CompareTag("Water"))
+        {
+            SceneManager.LoadScene("GameLose");
+        }
+    }
+}
